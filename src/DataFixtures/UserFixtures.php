5e11a47e004d6b8bd03f4eea5328dfbd90c69ec7
DylanTheAmerican <58289032+DylanTheAmerican@users.noreply.github.com>
2022-06-20 14:10:10 +0200

    Add User and Topic entity and fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class UserFixtures extends Fixture
+{
+    private Generator $faker;
+
+    public function __construct(private UserPasswordHasherInterface $passwordEncoder,
+    private SluggerInterface $slugger)
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $count = $this->faker->numberBetween(5, 20);
+        for ($a = 0; $a < $count; $a++) {
+            $this->createUser($manager);
+        }
+
+        $this->createUser($manager, [
+            'username' => 'Loken32',
+            'roles' => ['ROLE_AUTHOR', 'ROLE_ADMINISTRATOR'],
+            'password' => 'DylanCampion',
+            'firstname' => 'Dylan',
+            'lastname' => 'Campion',
+            'address' => 'Rue 12',
+            'postalcode' => '32600',
+            'city' => 'Isle Jourdain',
+            'email' => 'dylan.campion@gmail.com',
+            'phonenumber' => '0606060606',
+        ]);
+
+        $manager->flush();
+    }
+
+    public function createUser(ObjectManager $manager, array $data = [])
+    {
+        static $index = 0;
+
+        $data = array_replace(
+            [
+                'username' => $this->faker->userName(),
+                'roles' => ['ROLE_AUTHOR'],
+                'password' => $this->faker->password(),
+                'firstname' => $this->faker->firstName(),
+                'lastname' => $this->faker->lastName(),
+                'address' => $this->faker->address(),
+                'postalcode' => $this->faker->postcode(),
+                'city' => $this->faker->city(),
+                'email' => $this->faker->email(),
+                'phonenumber' => $this->faker->phoneNumber(),
+               
+            ],
+            $data,
+        );
+        $user = (new User())
+            ->setUsername($data['username'])
+            ->setRoles($data['roles'])
+            ->setPassword($data['password'])
+            ->setFirstname($data['firstname'])
+            ->setLastname($data['lastname'])
+            ->setAddress($data['address'])
+            ->setPostalcode($data['postalcode'])
+            ->setCity($data['city'])
+            ->setEmail($data['email'])
+            ->setPhonenumber($data['phonenumber']);
+            
+            // ->computeSlug($this->slugger)
+            
+
+        $user->setPassword($this->passwordEncoder->hashPassword($user, $data['password']));
+        $manager->persist($user);
+        $this->setReference('user-' . $index++, $user);
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $username;
+
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    private $password;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $firstname;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $lastname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $address;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $postalcode;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $city;
+
+    #[ORM\Column(type: 'string', length: 200)]
+    private $email;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $phonenumber;
+
+    #[ORM\OneToMany(mappedBy: 'author', targetEntity: Topic::class)]
+    private $topics;
+
+    public function __construct()
+    {
+        $this->topics = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getPostalcode(): ?string
+    {
+        return $this->postalcode;
+    }
+
+    public function setPostalcode(string $postalcode): self
+    {
+        $this->postalcode = $postalcode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhonenumber(): ?string
+    {
+        return $this->phonenumber;
+    }
+
+    public function setPhonenumber(string $phonenumber): self
+    {
+        $this->phonenumber = $phonenumber;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Topic>
+     */
+    public function getTopics(): Collection
+    {
+        return $this->topics;
+    }
+
+    public function addTopic(Topic $topic): self
+    {
+        if (!$this->topics->contains($topic)) {
+            $this->topics[] = $topic;
+            $topic->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTopic(Topic $topic): self
+    {
+        if ($this->topics->removeElement($topic)) {
+            // set the owning side to null (unless already changed)
+            if ($topic->getAuthor() === $this) {
+                $topic->setAuthor(null);
+            }
+        }
+
+        return $this;
+    }
+}
